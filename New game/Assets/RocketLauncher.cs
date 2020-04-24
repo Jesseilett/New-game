bb795618e18e2471dee15626cd8357f2b64a1fd6
Jesseilett <64066179+Jesseilett@users.noreply.github.com>
2020-04-24 17:53:07 +1000

    entire game project
    asdfghjkl

---
+++ New game/Assets/RocketLauncher.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic; 
+using UnityEngine;
+
+public class RocketLauncher : MonoBehaviour  
+{
+    public bool playerControlled = true;                                        // Is this rocket launcher controlled by the player? 
+    public float fireInterval = 3.0f;                                           // How many seconds between shots? 
+
+    private float fireTimer = 0;                                                // Keep track of when we can fire again 
+
+    public GameObject rocketPrefab;                                             // Reference to Rocket prefab so we spawn it 
+    public Vector3 spawnOffset;                                                 // A position offset for where the rocket is spawned so we dont spawn inside the gun 
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fireTimer = fireInterval;                                               // When the game starts we are ready to shoot
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        fireTimer += Time.deltaTime;                                            // Advance the timer 
+
+            if (fireTimer >= fireInterval)                                      // Have we waited long enough to shoot? 
+        {
+            if (playerControlled && Input.GetButtonDown ("Fire1"))              // Rocket is player controlled adn fire button pressed 
+            {
+                Fire();                                                         // Fire a rocket 
+            }
+        }
+    }
+
+    public void Fire() 
+    {
+        GameObject rocketInstance = Instantiate(rocketPrefab);                   //Spawn and store a new rocket prefab 
+
+        rocketInstance.transform.position = transform.position + spawnOffset;    // Apply the spawn offset relative to the gun position 
+        rocketInstance.transform.rotation = transform.rotation;                  // Rotate rocket to match guns rotation 
+
+        fireTimer = 0;                                                           // Reset fire timer 
+    }
+}
