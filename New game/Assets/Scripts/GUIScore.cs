d0fd4287d5b410975192ec914bbd13be3b8e479e
Jesseilett <64066179+Jesseilett@users.noreply.github.com>
2020-05-12 20:57:28 +1000

    Made a hud and gui
    But the ingame quit button doesn't work quite right (You have to press the button then space to exit the game).

---
+++ New game/Assets/Scripts/GUIScore.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GUIScore : MonoBehaviour
+{
+    public Text _scoreLabel = null;
+    private int _score = 0;
+
+    public int GetScore()
+    {
+        return _score; 
+    }
+    public void AddScore(int value)
+    {
+        _score += value;
+        _scoreLabel.text = _score.ToString(); 
+    }
+}
+
+   
+
---
+++ New game/Assets/Scripts/ScoreOnClick.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class ScoreOnClick : MonoBehaviour
+{
+    public GUIScore _score = null;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) == true)
+        {
+            _score.AddScore(1); 
+        }
+    }
+}
